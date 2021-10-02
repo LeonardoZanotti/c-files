f200c9868880a7d803fc884732375ae8279a3e46
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-10-02 16:05:45 -0300

    asc II

--- ASCII Table/ascII-table.c
+++ ASCII Table/ascII-table.c
@@ -1,4 +1,37 @@
-for (int i = 1; i < 10; i++)
+#include <stdio.h>
+
+int main()
 {
-    System.out.println((i % 2 == 0) ? i : '\t' + Integer.toString(i));
+    // First row -> hex label
+    printf("\t");
+    for (int j = 0; j <= 15; j++)
+    {
+        if (j == 15)
+        {
+            printf("%x \n", j);
+            continue;
+        }
+        printf("%x \t", j);
+    }
+
+    // ASCII values
+    for (int i = 0; i < 256; i++)
+    {
+        int integer = (i < 32) ? 32 : i;
+        char breakLine = ' ';
+
+        if (i % 16 == 15)
+        {
+            breakLine = '\n';
+        }
+
+        if (i % 16 == 0)
+        {
+            printf("%x \t %c", (i / 16), (char)integer);
+            continue;
+        }
+
+        printf("\t %c %c", (char)integer, breakLine);
+    }
+    return 0;
 }
