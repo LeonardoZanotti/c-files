3923d8b2841b7e9caffe028950bc0e88d39bb12c
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-10-03 10:26:13 -0300

    more functions

--- Static-list/static-list.c
+++ Static-list/static-list.c
@@ -20,6 +20,72 @@ int insert_end(List_type *v, int data)
     return 0;
 }
 
+// Remove element from the list by index
+int remove_index(List_type *v, int index)
+{
+    int i, data;
+    if ((*v).end != 0)
+    {
+        if ((index >= 0) && (index < (*v).end))
+        {
+            if (index == 0)
+            {
+                if (exclui_inicio_lista(v, &data) == 1)
+                    printf("\nThe element %d has been removed from index %d", data, index);
+                return 1;
+            }
+            else if (index == (*v).end - 1)
+            {
+                if (exclui_fim_lista(v, &data) == 1)
+                    printf("\nThe element %d has been removed from index %d", data, index);
+                return 1;
+            }
+            else
+            {
+                data = (*v).data[index];
+                for (i = index; i < (*v).end; i++)
+                    (*v).data[i] = (*v).data[i + 1];
+                (*v).end--;
+                printf("\nThe element %d has been removed from index %d", data, index);
+                return 1;
+            }
+        }
+        return -1;
+    }
+    return 0;
+}
+
+// Find data by content
+int search_by_content(List_type v, int data, int *index)
+{
+    int i, found = 0;
+    for (i = 0; i < v.end - 1; i++)
+    {
+        if (v.data[i] == data)
+        {
+            *index = i;
+            found = 1;
+            break;
+        }
+    }
+    if (found)
+        return 1;
+    return 0;
+}
+
+// Print all the list data
+int print_list(List_type v)
+{
+    int i;
+    if (v.end != 0)
+    {
+        for (i = 0; i < v.end; i++)
+            printf("\n%d", v.data[i]);
+        return 1;
+    }
+    return 0;
+}
+
 int main()
 {
     return 0;
@@ -33,7 +99,4 @@ int main()
 // Inserir no meio(ordenado)
 // Remover do início
 // Remover do fim
-// Remover do meio
 // Consultar pelo conteúdo
-// Consultar pela posição
-// Imprimir Lista(Esta operação não foi apresentada pelo prof.André.)
