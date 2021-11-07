b5f140ae886a93bfc815bef60e8ca1b1f2259664
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-11-07 17:25:38 -0300

    finished dynamic stack

--- Dynamic-stack/dynamic-stack.c
+++ Dynamic-stack/dynamic-stack.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_STACK 5
-
-typedef struct Stack
+typedef struct StackBase StackNode;
+typedef StackNode *Stack;
+struct StackBase
 {
-    int data[MAX_STACK];
-    int length;
-} Stack_type;
-
-// Auxiliar method
-// Verify if stack is full
-int full_stack(Stack_type *v)
-{
-    if (v == NULL)
-        return -1;
-    return (*v).length == MAX_STACK;
-}
+    int data;
+    StackNode *next;
+};
 
 // Auxiliar method
 // Verify if stack is empty
-int empty_stack(Stack_type *v)
+int empty_stack(Stack *v)
 {
-    if (v == NULL)
-        return -1;
-    return (*v).length == 0;
+    return (v == NULL || (*v) == NULL) ? 1 : 0;
+}
+
+// Auxiliar method
+// Check stack length
+int stack_length(Stack *v)
+{
+    if (empty_stack(v))
+        return 0;
+    int count = 0;
+    StackNode *N = (*v);
+    while (N != NULL)
+    {
+        count++;
+        N = (*N).next;
+    }
+    return count;
 }
 
 // Create stack
-Stack_type *create_stack()
+Stack *create_stack()
 {
-    Stack_type *stack = (Stack_type *)malloc(sizeof(Stack_type));
+    Stack *stack = (Stack *)malloc(sizeof(Stack));
 
     if (stack != NULL)
     {
-        (*stack).length = 0;
+        *stack = NULL;
     }
 
     printf("\nStack created!\n");
@@ -43,54 +48,71 @@ Stack_type *create_stack()
 }
 
 // Free the memory allocated to the stack
-void free_stack(Stack_type *v)
+void free_stack(Stack *v)
 {
-    free(v);
+    if (v != NULL)
+    {
+        StackNode *N;
+        while ((*v) != NULL)
+        {
+            N = (*v);
+            *v = (*N).next;
+            free(N);
+        }
+        free(v);
+    }
     printf("\nCleared stack from memory\n");
 }
 
 // Insert in the start of the stack
-int insert_start(Stack_type *v, int data)
+int insert_start(Stack *v, int data)
 {
-    if (!full_stack(v))
-    {
-        (*v).data[(*v).length++] = data;
-        printf("Inserted %d at the start of the stack\n", data);
-        return 1;
-    }
-    return 0;
+    if (v == NULL)
+        return 0;
+    StackNode *N = (StackNode *)malloc(sizeof(StackNode));
+    if (N == NULL)
+        return 0;
+    (*N).data = data;
+    (*N).next = (*v);
+    (*v) = N;
+    printf("Inserted %d at the start of the stack\n", data);
+    return 1;
 }
 
 // Remove element from the start of the stack
-int remove_start(Stack_type *v)
+int remove_start(Stack *v)
 {
     if (empty_stack(v))
         return 0;
-    (*v).length--;
+    StackNode *N = (*v);
+    (*v) = (*N).next;
+    free(N);
     printf("Removed element from start of stack\n");
     return 1;
 }
 
 // Get element from the top of the stack
-int get_top_of_stack(Stack_type *v, int *content)
+int get_top_of_stack(Stack *v, int *content)
 {
     if (empty_stack(v))
         return 0;
-    *content = (*v).data[(*v).length - 1];
+    *content = (*v)->data;
     return 1;
 }
 
 // Print all the stack data
-int print_stack(Stack_type *v)
+int print_stack(Stack *v)
 {
-    int i;
-
     if (!empty_stack(v))
     {
         printf("\nStack:");
 
-        for (i = (*v).length - 1; i >= 0; i--)
-            printf("\n%d", (*v).data[i]);
+        StackNode *N = (*v);
+        while (N != NULL)
+        {
+            printf("\n%d", (*N).data);
+            N = (*N).next;
+        }
 
         printf("\n");
 
@@ -104,7 +126,7 @@ int main()
 {
     char option[3];
     int optionInt = 0;
-    Stack_type *stack;
+    Stack *stack;
 
     char input[3];
     int content;
@@ -113,8 +135,8 @@ int main()
     {
         optionInt = 0;
 
-        printf("\n1) Create static stack\n");
-        printf("2) Free static stack\n");
+        printf("\n1) Create dynamic stack\n");
+        printf("2) Free dynamic stack\n");
         printf("3) Insert at the start\n");
         printf("6) Remove from the start\n");
         printf("9) Find the top\n");
