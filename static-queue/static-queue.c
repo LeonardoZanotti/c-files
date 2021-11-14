226b8b4ca776aa6a6ed5e94d3706b456dfd12fbb
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-11-14 18:33:57 -0300

    insert, remove and get first

--- static-queue/static-queue.c
+++ static-queue/static-queue.c
@@ -37,16 +37,6 @@ int empty_queue(Queue *q)
 }
 
 // Auxiliar method
-// Print the results of the search
-void print_search_result(int searchResult)
-{
-    if (searchResult != -1)
-    {
-        printf("Found item: %d\n", searchResult);
-    }
-}
-
-// Auxiliar method
 // Read param value to use in the functions
 void read_param(int *param)
 {
@@ -80,24 +70,6 @@ void free_queue(Queue *q)
     printf("\nCleared queue from memory\n");
 }
 
-// Insert in the start of the queue
-int insert_start(Queue *q, int data)
-{
-    if (!full_queue(q))
-    {
-        for (int i = (*q).end - 1; i >= 0; i--)
-            (*q).data[i + 1] = (*q).data[i];
-
-        (*q).data[0] = data;
-        (*q).end++;
-
-        printf("Inserted %d at the start of the queue\n", data);
-
-        return 1;
-    }
-    return 0;
-}
-
 // Insert in the end of the queue
 int insert_end(Queue *q, int data)
 {
@@ -105,38 +77,9 @@ int insert_end(Queue *q, int data)
     {
         (*q).data[(*q).end] = data;
         (*q).end++;
+        (*q).length++;
 
         printf("Inserted %d at the end of the queue\n", data);
-
-        return 1;
-    }
-    return 0;
-}
-
-// Insert item sorted in the queue
-int insert_sorted(Queue *q, int data)
-{
-    if (!full_queue(q))
-    {
-        int k = 0, i = 0;
-
-        while (k < (*q).end && (*q).data[k] < data)
-        {
-            k++;
-        }
-
-        if ((*q).end != 0)
-        {
-            for (i = (*q).end - 1; i >= k; i--)
-            {
-                (*q).data[i + 1] = (*q).data[i];
-            }
-        }
-
-        (*q).data[k] = data;
-        (*q).end++;
-
-        printf("Inserted %d at in the queue sorted\n", data);
 
         return 1;
     }
@@ -147,9 +90,7 @@ int insert_sorted(Queue *q, int data)
 int remove_start(Queue *q)
 {
     if (empty_queue(q))
-    {
         return 0;
-    }
 
     for (int i = 0; i < (*q).end; i++)
     {
@@ -157,95 +98,19 @@ int remove_start(Queue *q)
     }
 
     (*q).end--;
+    (*q).length--;
 
     printf("Removed element from start of queue\n");
 
     return 1;
 }
 
-// Remove element from the end of the queue
-int remove_end(Queue *q)
+// Get first element of the queue
+int get_first(Queue *q)
 {
     if (empty_queue(q))
-    {
         return 0;
-    }
-
-    (*q).end--;
-
-    printf("Removed element from end of queue\n");
-
-    return 1;
-}
-
-// Remove element from the middle of the queue by index
-int remove_middle(Queue *q, int index)
-{
-    int i, data;
-    if (!empty_queue(q))
-    {
-        if ((index >= 0) && (index < (*q).end))
-        {
-            if (index == 0)
-            {
-                remove_start(q);
-                return 1;
-            }
-            else if (index == (*q).end - 1)
-            {
-                remove_end(q);
-                return 1;
-            }
-            else
-            {
-                data = (*q).data[index];
-
-                for (i = index; i < (*q).end; i++)
-                    (*q).data[i] = (*q).data[i + 1];
-
-                (*q).end--;
-
-                printf("\nThe element %d has been removed from index %d\n", data, index);
-
-                return 1;
-            }
-        }
-        return -1;
-    }
-    return 0;
-}
-
-// Find data by content
-int search_by_content(Queue *q, int data, int *index)
-{
-    int i, found = 0;
-
-    for (i = 0; i < (*q).end - 1; i++)
-    {
-        if ((*q).data[i] == data)
-        {
-            *index = i;
-            found = 1;
-            break;
-        }
-    }
-
-    if (found)
-        return 1;
-
-    return 0;
-}
-
-// Find data by index
-int search_by_index(Queue *q, int *data, int index)
-{
-    if (q == NULL || index < 0 || index >= (*q).end)
-    {
-        return 0;
-    }
-
-    *data = (*q).data[index];
-
+    printf("First item of the queue is: %d\n", (*q).data[(*q).start]);
     return 1;
 }
 
@@ -277,7 +142,7 @@ int main()
     int searchResult;
     int param;
 
-    while (optionInt != 12)
+    while (optionInt != 11)
     {
         optionInt = 0;
 
@@ -289,10 +154,9 @@ int main()
         printf("6) Remove from the start\n");
         printf("7) Remove from the end\n");
         printf("8) Remove from the middle\n");
-        printf("9) Find by content\n");
-        printf("10) Find by index\n");
-        printf("11) Print the queue\n");
-        printf("12) Exit\n");
+        printf("9) Get first item\n");
+        printf("10) Print the queue\n");
+        printf("11) Exit\n");
 
         while (!(optionInt >= 1 && optionInt <= 12))
         {
@@ -332,19 +196,12 @@ int main()
             remove_middle(queue, param);
             break;
         case 9:
-            read_param(&param);
-            search_by_content(queue, param, &searchResult);
-            print_search_result(searchResult);
+            get_first(queue);
             break;
         case 10:
-            read_param(&param);
-            search_by_index(queue, &searchResult, param);
-            print_search_result(searchResult);
-            break;
-        case 11:
             print_queue(queue);
             break;
-        case 12:
+        case 11:
             free_queue(queue);
             printf("\nExiting...\n");
             break;
