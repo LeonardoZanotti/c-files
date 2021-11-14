31b1aa067c0bc3c7d153333b7a82c51c80acedb1
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-11-14 18:49:20 -0300

    updates in the structure of functions

--- Dynamic queue/dynamic-queue.c
+++ Dynamic queue/dynamic-queue.c
@@ -43,16 +43,6 @@ int size_queue(Queue *L)
 }
 
 // Auxiliar method
-// Print the results of the search
-void print_search_result(int searchResult, int response)
-{
-    if (response)
-        printf("Found item: %d\n", searchResult);
-    else
-        printf("Not found!\n");
-}
-
-// Auxiliar method
 // Read param value to use in the functions
 void read_param(int *param)
 {
@@ -88,24 +78,6 @@ void free_queue(Queue *L)
     printf("\nCleared queue from memory\n");
 }
 
-// Insert in the start of the queue
-int insert_start(Queue *L, int data)
-{
-    if (L == NULL)
-        return 0;
-    NodeData *N = (NodeData *)malloc(sizeof(NodeData));
-    if (N == NULL)
-        return 0;
-    (*N).data = data;
-    (*N).next = (*L).start;
-    if ((*L).start == NULL)
-        (*L).end = N;
-    (*L).start = N;
-    (*L).length++;
-    printf("Inserted %d at the start of the queue\n", data);
-    return 1;
-}
-
 // Insert in the end of the queue
 int insert_end(Queue *L, int data)
 {
@@ -123,53 +95,6 @@ int insert_end(Queue *L, int data)
     (*L).end = N;
     (*L).length++;
     printf("Inserted %d at the end of the queue\n", data);
-    return 1;
-}
-
-// Insert item sorted in the queue
-int insert_sorted(Queue *L, int data)
-{
-    if (L == NULL)
-        return 0;
-
-    NodeData *N = (NodeData *)malloc(sizeof(NodeData));
-    if (N == NULL)
-        return 0;
-
-    (*N).data = data;
-
-    if (empty_queue(L))
-    {
-        (*N).next = NULL;
-        (*L).start = N;
-    }
-    else
-    {
-        NodeData *previous, *actual = (*L).start;
-
-        while (actual != NULL && (*actual).data < data)
-        {
-            previous = actual;
-            actual = (*actual).next;
-        }
-
-        if (actual == (*L).start)
-        {
-            (*N).next = (*L).start;
-            (*L).start = N;
-        }
-        else
-        {
-            if ((*previous).next == NULL)
-                (*L).end = N;
-            (*N).next = (*previous).next;
-            (*previous).next = N;
-        }
-    }
-
-    (*L).length++;
-    printf("Inserted %d at in the queue sorted\n", data);
-
     return 1;
 }
 
@@ -192,79 +117,8 @@ int remove_start(Queue *L)
     return 1;
 }
 
-// Remove element from the end of the queue
-int remove_end(Queue *L)
-{
-    if (empty_queue(L))
-    {
-        return 0;
-    }
-
-    NodeData *previous, *N = (*L).start;
-
-    while ((*N).next != NULL)
-    {
-        previous = N;
-        N = (*N).next;
-    }
-
-    if (N == (*L).start)
-    {
-        (*L).start = NULL;
-        (*L).end = NULL;
-    }
-    else
-    {
-        (*previous).next = (*N).next;
-        (*L).end = previous;
-    }
-
-    (*L).length--;
-    free(N);
-
-    printf("Removed element from end of queue\n");
-
-    return 1;
-}
-
-// Remove element from the middle of the queue by index
-int remove_middle(Queue *L, int index)
-{
-    if (empty_queue(L) || index >= size_queue(L))
-    {
-        return 0;
-    }
-
-    NodeData *previous, *N = (*L).start;
-
-    for (int i = 0; i < index; i++)
-    {
-        previous = N;
-        N = (*N).next;
-    }
-
-    if (N == (*L).start)
-    {
-        remove_start(L);
-    }
-    else if (N == (*L).end)
-    {
-        remove_end(L);
-    }
-    else
-    {
-        (*previous).next = (*N).next;
-        (*L).length--;
-        printf("\nThe element %d has been removed from index %d\n", (*N).data, index);
-    }
-
-    free(N);
-
-    return 1;
-}
-
-// Find data by content
-int search_by_content(Queue *L, int data, int *index)
+// Get first element of the queue
+int get_first(Queue *L)
 {
     if (empty_queue(L))
         return 0;
@@ -289,23 +143,6 @@ int search_by_content(Queue *L, int data, int *index)
     return 0;
 }
 
-// Find data by index
-int search_by_index(Queue *L, int *data, int index)
-{
-    if (empty_queue(L) || index < 0 || index >= size_queue(L))
-    {
-        return 0;
-    }
-
-    NodeData *N = (*L).start;
-    for (int i = 0; i < index; i++)
-        N = (*N).next;
-
-    *data = (*N).data;
-
-    return 1;
-}
-
 // Print all the queue data
 int print_queue(Queue *L)
 {
@@ -327,24 +164,19 @@ int main()
     int searchResult;
     int param;
 
-    while (optionInt != 12)
+    while (optionInt != 7)
     {
         optionInt = 0;
 
-        printf("\n1) Create chained header queue\n");
-        printf("2) Free chained header queue\n");
-        printf("3) Insert at the start\n");
+        printf("\n1) Create queue\n");
+        printf("2) Free queue\n");
         printf("4) Insert at the end\n");
-        printf("5) Insert sorted\n");
         printf("6) Remove from the start\n");
-        printf("7) Remove from the end\n");
-        printf("8) Remove from the middle\n");
-        printf("9) Find by content\n");
-        printf("10) Find by index\n");
+        printf("10) Get first item\n");
         printf("11) Print the queue\n");
         printf("12) Exit\n");
 
-        while (!(optionInt >= 1 && optionInt <= 12))
+        while (!(optionInt >= 1 && optionInt <= 7))
         {
             printf("\nChoose an option: ");
             scanf("%s", option);
@@ -361,40 +193,18 @@ int main()
             break;
         case 3:
             read_param(&param);
-            insert_start(queue, param);
-            break;
-        case 4:
-            read_param(&param);
             insert_end(queue, param);
             break;
-        case 5:
-            read_param(&param);
-            insert_sorted(queue, param);
-            break;
-        case 6:
+        case 4:
             remove_start(queue);
             break;
-        case 7:
-            remove_end(queue);
+        case 5:
+            get_first(queue);
             break;
-        case 8:
-            read_param(&param);
-            remove_middle(queue, param);
-            break;
-        case 9:
-            read_param(&param);
-            int contentResponse = search_by_content(queue, param, &searchResult);
-            print_search_result(searchResult, contentResponse);
-            break;
-        case 10:
-            read_param(&param);
-            int indexResponse = search_by_index(queue, &searchResult, param);
-            print_search_result(searchResult, indexResponse);
-            break;
-        case 11:
+        case 6:
             print_queue(queue);
             break;
-        case 12:
+        case 7:
             free_queue(queue);
             printf("\nExiting...\n");
             break;
